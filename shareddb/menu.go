988d08d23457e5d3eeb715eb8d6b6e82f163fe4a
batbold <burabatbold2@gmail.com>
2025-02-07 14:41:25 +0800

    [update] added veritech item id in product

--- shareddb/menu.go
+++ shareddb/menu.go
@@ -4,11 +4,12 @@ type (
 	BaseProduct struct {
 		Base
 		BaseOrg
-		Name         string  `gorm:"->;type:varchar(1)" json:"name"`
-		VeritechCode string  `gorm:"column:veritech_code" json:"veritech_code"` //
-		ImagePath    string  `gorm:"column:image_path" json:"image_path"`       // Зураг         //
-		Measurement  string  `gorm:"column:measurement" json:"measurement"`     // Хэмжих нэгж
-		Price        float64 `gorm:"column:price" json:"price"`                 // Vending машин дээр харагдах үнэ (нийт)
-		IsActive     bool    `gorm:"column:is_active" json:"is_active"`         // Бүтээгдэхүүн идэвхтэй эсэх
+		Name           string  `gorm:"->;type:varchar(1)" json:"name"`
+		VeritechCode   string  `gorm:"column:veritech_code" json:"veritech_code"`             //
+		ImagePath      string  `gorm:"column:image_path" json:"image_path"`                   // Зураг         //
+		Measurement    string  `gorm:"column:measurement" json:"measurement"`                 // Хэмжих нэгж
+		Price          float64 `gorm:"column:price" json:"price"`                             // Vending машин дээр харагдах үнэ (нийт)
+		IsActive       bool    `gorm:"column:is_active" json:"is_active"`                     // Бүтээгдэхүүн идэвхтэй эсэх
+		VeritechItemID string  `gorm:"column:veritech_item_id;index" json:"veritech_item_id"` // Veritech-ийн item_id
 	}
 )
